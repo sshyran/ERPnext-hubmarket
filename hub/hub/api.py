52777e03b2103b1d53af8d1a9222bd7775ca204c
pratu16x7 <pratu16x7@gmail.com>
2017-08-18 21:24:24 +0530

    first cut

--- hub/hub/api.py
+++ hub/hub/api.py
@@ -1,75 +1,115 @@
 # Copyright (c) 2015, Web Notes Technologies Pvt. Ltd. and Contributors and contributors
 # For license information, please see license.txt
 
-import frappe, json
+import frappe, json, requests, os
 from frappe.utils import now
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.serialization import load_pem_private_key, load_pem_public_key
+
+user_profile_fields = ["enabled", "hub_user_name", "email", "company", "country", "public_key_pem"]
+seller_fields = ["publish", "seller_website", "seller_city", "seller_description"]
+
+private_files = frappe.get_site_path('private', 'files')
+public_key_path = os.path.join(private_files, "hub_rsa.pub")
+private_key_path = os.path.join(private_files, "hub_rsa")
+
+if os.path.exists(public_key_path):
+	with open(public_key_path, "rb") as public_key_file:
+		hub_public_key_pem = public_key_file.read()
 
 @frappe.whitelist(allow_guest=True)
-def register(enabled, hub_user_name, email, company, country, publish, seller_website=None, seller_city=None,
-		seller_description=None):
+def register(args_data):
 	"""Register on the hub."""
+	args = json.loads(args_data)
+	if frappe.get_all("Hub User", filters = {"email": args["email"]}):
+		return
+
 	hub_user = frappe.new_doc("Hub User")
-	for key in ("enabled", "hub_user_name", "email", "company", "country", "publish", "seller_website", "seller_city", "seller_description"):
-		hub_user.set(key, locals()[key])
+	for key in user_profile_fields:
+		hub_user.set(key, args[key])
+
 	hub_user.insert(ignore_permissions=True)
-	return hub_user.as_dict()
+	response = hub_user.as_dict()
+	response["hub_public_key_pem"] = hub_public_key_pem
+	return response
 
 @frappe.whitelist(allow_guest=True)
-def update_user_details(password, args):
+def unregister(password):
+	# Delete all of user's transactions and items
+	print "////////unregister////////"
 	hub_user = get_user(password)
-	for key, val in json.loads(args).iteritems():
-		hub_user.set(key, val)
-	hub_user.save(ignore_permissions=True)
+
+	# last step
+	frappe.delete_doc('Hub User', hub_user.name, ignore_permissions=True)
 
 @frappe.whitelist(allow_guest=True)
-def unpublish(password):
+def unpublish(access_token):
 	"""Un publish seller"""
-	hub_user = get_user(password)
-	if hub_user:
-		hub_user.publish = 0
-		hub_user.save(ignore_permissions=True)
+	seller = get_seller(access_token)
+	seller.published=0
+	seller.save(ignore_permissions=True)
 
 @frappe.whitelist(allow_guest=True)
-def delete(password, item_code):
-	"""Delete item on portal"""
+def call_method(password, method, message):
 	hub_user = get_user(password)
-	item = frappe.db.get_value("Hub Item", {"item_code": item_code, "hub_user_name": hub_user.name})
-	if item:
-		frappe.delete_doc("Hub Item", item)
+	args = json.loads(message)
+	return getattr(hub_user, method)(args)
 
 @frappe.whitelist(allow_guest=True)
-def sync(password, items_to_update, item_list):
-	"""Sync new items"""
+def decrypt_message_and_call_method(password, method, signature, encrypted_key, message):
+	print type(encrypted_key)
+	print len(encrypted_key)
+	en_key = str(encrypted_key.encode('latin-1'))
+
+	print en_key
+	print len(en_key)
+
 	hub_user = get_user(password)
+	user_public_key = load_pem_public_key(
+		str(hub_user.public_key_pem),
+		backend=default_backend()
+	)
 
-	all_items = frappe.db.sql_list("select item_code from `tabHub Item` where hub_user_name=%s", hub_user.name)
-	item_list = json.loads(item_list)
-	for item in all_items:
-		if item not in item_list:
-			frappe.delete_doc("Hub Item", item)
-	
-	# insert / update items
-	for item in json.loads(items_to_update):
-		item_code = frappe.db.get_value("Hub Item",
-			{"hub_user_name": hub_user.name, "item_code": item.get("item_code")})
-		if item_code:
-			hub_item = frappe.get_doc("Hub Item", item_code)
-		else:
-			hub_item = frappe.new_doc("Hub Item")
-			hub_item.hub_user_name = hub_user.name
+	# Verify key
+	user_public_key.verify(
+		signature,
+		str(encrypted_key),
+		padding.PSS(
+			mgf=padding.MGF1(hashes.SHA256()),
+			salt_length=padding.PSS.MAX_LENGTH
+		),
+		hashes.SHA256()
+	)
 
-		for key in ("item_code", "item_name", "description", "image", "item_group", "price", "stock_qty", "stock_uom"):
-			hub_item.set(key, item.get(key))
+	# Decrypt key
+	if os.path.exists(private_key_path):
+		with open(private_key_path, "rb") as private_key_file:
+			private_key = serialization.load_pem_private_key(
+				private_key_file.read(),
+				password=None,
+				backend=default_backend()
+			)
 
-		for key in ("hub_user_name", "email", "country", "seller_city"):
-			hub_item.set(key, hub_user.get(key))
+	key = private_key.decrypt(
+		en_key,
+		padding.OAEP(
+			mgf=padding.MGF1(algorithm=hashes.SHA1()),
+			algorithm=hashes.SHA1(),
+			label=None
+		)
+	)
 
-		hub_item.published = 1
-		hub_item.save(ignore_permissions=True)
-	return frappe._dict({"last_sync_datetime":now()})
+	# Decrypt message
+	f = Fernet(key)
+	plaintext = f.decrypt(str(message))
+	args = json.loads(plaintext.decode("utf-8"))
+	getattr(hub_user, method)(args)
 
 @frappe.whitelist(allow_guest=True)
-def get_items(password, text=None, country=None, start=0, limit=50):
+def get_items(password, text=None, category=None, seller=None, country=None, start=0, limit=50):
 	"""Returns list of items by filters"""
 	get_user(password)
 	or_filters = [
@@ -79,11 +119,80 @@ def get_items(password, text=None, country=None, start=0, limit=50):
 	filters = {
 		"published": "1"
 	}
+	if category:
+		filters["item_group"] = category
+	if seller:
+		filters["hub_user_name"] = seller
 	if country:
 		filters["country"] = country
-	return frappe.get_all("Hub Item", fields=["item_code", "item_name", "description", "item_group", "image", 
-		"price", "stock_qty", "stock_uom", "hub_user_name", "email", "country", "seller_city"],
+	return frappe.get_all("Hub Item", fields=["item_code", "item_name", "item_group", "description", "image",
+		"hub_user_name", "email", "country", "seller_city", "company", "seller_website"],
 			filters=filters, or_filters=or_filters, limit_start = start, limit_page_length = limit)
 
+@frappe.whitelist(allow_guest=True)
 def get_user(password):
 	return frappe.get_doc("Hub User", {"password": password})
+
+@frappe.whitelist(allow_guest=True)
+def get_all_users():
+	return frappe.get_all("Hub User", fields=["hub_user_name", "country"])
+
+@frappe.whitelist(allow_guest=True)
+def get_categories():
+	return frappe.get_all("Hub Category", fields=["category_name"])
+
+@frappe.whitelist(allow_guest=True)
+def get_user_details(password, user_name):
+	return frappe.get_doc("Hub User", {"hub_user_name": user_name})
+
+
+
+
+
+
+
+# TESTER
+@frappe.whitelist(allow_guest=True)
+def test():
+	user = "http://erpnext.reinstall:8000"
+	response = requests.get(user + "/api/method/erpnext.hub_node.api.test")
+	response.raise_for_status()
+	return response.json().get("message")
+
+@frappe.whitelist(allow_guest=True)
+def load_message(password, msg_type, sender, receiver, receiver_website, msg_data):
+	msg = frappe.new_doc("Hub Message")
+	msg.message_type = msg_type
+	if msg_type == "Request for Quotation":
+		msg.naming_series = "DAT-RFQ-"
+	else:
+		msg.naming_series = "DAT-OPP-"
+
+	msg.sender = sender
+	msg.receiver = receiver
+	msg.receiver_website = receiver_website
+
+	msg.message_body = msg_data
+
+	msg.save(ignore_permissions=True)
+	frappe.db.commit()
+
+	send_messages()
+
+def send_messages():
+	for msg in frappe.get_all("Hub Message", fields=["message_type", "receiver_website", "message_body"]):
+		send_message(msg)
+
+def send_message(message):
+	msg_type = message["message_type"]
+	receiver_website = message["receiver_website"]
+	msg_data = message["message_body"]
+	response = requests.get(receiver_website + "/api/method/erpnext.hub_node.api.make", data={
+				"msg_type": msg_type,
+				"data": msg_data
+			})
+	response.raise_for_status()
+	return response.json().get("message")
+
+def store_keys():
+	pass
--- hub/hub/doctype/hub_user/hub_user.py
+++ hub/hub/doctype/hub_user/hub_user.py
@@ -3,10 +3,62 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, json
 from frappe.model.document import Document
-from frappe.utils import random_string
+from frappe.utils import random_string, now
 
 class HubUser(Document):
 	def autoname(self):
 		self.password = random_string(16)
+
+	def update_user_details(self, args):
+		self.old = frappe.get_doc('Hub User', self.name)
+		for key, new_value in args.iteritems():
+			old_value = self.old.get(key)
+			if(new_value != old_value):
+				self.set(key, new_value)
+
+	def unpublish(self):
+		"""Un publish seller, delete items"""
+		self.publish = 0
+
+	def delete(self, item_code):
+		"""Delete item on portal"""
+		item = frappe.db.get_value("Hub Item", {"item_code": item_code, "hub_user_name": self.name})
+		if item:
+			frappe.delete_doc("Hub Item", item)
+
+	def unregister(self):
+		"""Unregister user"""
+		pass
+
+	def sync(self, args):
+		"""Sync new items"""
+		all_items = frappe.db.sql_list("select item_code from `tabHub Item` where hub_user_name=%s", self.name)
+		item_list = json.loads(args["item_list"])
+		for item in all_items:
+			if item not in item_list:
+				frappe.delete_doc("Hub Item", item)
+
+		item_fields = ["item_code", "item_name", "description", "image",
+			"item_group", "price", "stock_qty", "stock_uom"]
+
+		# insert / update items
+		for item in json.loads(args["items_to_update"]):
+			item_code = frappe.db.get_value("Hub Item",
+				{"hub_user_name": self.name, "item_code": item.get("item_code")})
+			if item_code:
+				hub_item = frappe.get_doc("Hub Item", item_code)
+			else:
+				hub_item = frappe.new_doc("Hub Item")
+				hub_item.hub_user_name = self.name
+
+			for key in item_fields:
+				hub_item.set(key, item.get(key))
+
+			for key in ("hub_user_name", "email", "country", "seller_city", "company", "seller_website"):
+				hub_item.set(key, self.get(key))
+
+			hub_item.published = 1
+			hub_item.save(ignore_permissions=True)
+		return frappe._dict({"last_sync_datetime":now()})
