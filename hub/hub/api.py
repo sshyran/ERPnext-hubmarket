875d2cd337c90180a9e345ef1ddc1344ad1864ac
Prateeksha Singh <pratu16x7@gmail.com>
2018-02-23 13:30:41 +0530

    keep out frappe dependent changes temporarily

--- hub/hub/api.py
+++ hub/hub/api.py
@@ -6,7 +6,7 @@ import frappe, json
 from frappe import _
 from frappe.utils import now, add_years, random_string
 from frappe.website.utils import is_signup_enabled
-from frappe.core.doctype.user.user import check_for_spamming, create_user
+# from frappe.core.doctype.user.user import check_for_spamming, create_user
 
 seller_fields = ["site_name", "seller_city", "seller_description"]
 publishing_fields = ["publish", "publish_pricing", "publish_availability"]
@@ -31,8 +31,29 @@ def sign_up(email, full_name, redirect_to):
 		else:
 			return 0, _("Already Registered")
 	else:
-		check_for_spamming()
-		user = create_user(email, full_name)
+		if frappe.db.sql("""select count(*) from tabUser where
+			HOUR(TIMEDIFF(CURRENT_TIMESTAMP, TIMESTAMP(modified)))=1""")[0][0] > 300:
+
+			frappe.respond_as_web_page(_('Temperorily Disabled'),
+				_('Too many users signed up recently, so the registration is disabled. Please try back in an hour'),
+				http_status_code=429)
+
+		from frappe.utils import random_string
+		user = frappe.get_doc({
+			"doctype":"User",
+			"email": email,
+			"first_name": full_name,
+			"enabled": 1,
+			"new_password": random_string(10),
+			"user_type": "Website User"
+		})
+		user.flags.ignore_permissions = True
+		user.insert()
+
+		# set default signup role as per Portal Settings
+		default_role = frappe.db.get_value("Portal Settings", None, "default_role")
+		if default_role:
+			user.add_roles(default_role)
 
 		if redirect_to:
 			frappe.cache().hset('redirect_after_login', user.name, redirect_to)
