27de1bae035422cbf3632ef4fdc21641c58395e7
Achilles Rasquinha <achillesrasquinha@gmail.com>
2018-02-25 00:12:57 +0530

    [ENH] added ping-pong for socket.io

--- hub/node/index.js
+++ hub/node/index.js
@@ -1,12 +1,16 @@
 
 // FrappeJS
 // Do something awesome!
-const frappe   = require("frappe");
-const hub    = { };
+const frappe   = require("frappe")
+const hub    = { }
 
 frappe.socket.on("connection", (socket) =>
 {
-    socket.on("ping", () => socket.emit("pong"));
-});
+    // convention - [app].[domain].[object]:[action]
+    socket.on("hub:ping", function () {
+        console.log(`PING: ${socket.request.headers.origin}`)
+        socket.emit("hub:pong", "pong")
+    })
+})
 
-module.exports = hub;
+module.exports = hub
