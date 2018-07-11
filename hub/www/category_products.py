8b55c4e4210e79adbe3626fedacb26f9b1d9487f
Akshar Raaj <akshar@agiliq.com>
2018-07-11 11:38:42 +0530

    Added pagination on category filtered products page

--- hub/www/category_products.py
+++ hub/www/category_products.py
@@ -1,10 +1,15 @@
 import frappe
 
+from hub.paginator import Paginator
+
 def get_context(context):
     category_name = frappe.local.request.args['category_name']
     fields = ['published', 'route', 'image', 'name', 'company_name', 'price', 'stock_qty', 'currency']
     filters = {'published': 1, 'hub_category': category_name}
-    context.items = frappe.get_list('Hub Item', fields=fields, filters=filters, start=0, limit=20)
+    page_number = int(frappe.local.request.args.get('page_number', 1))
+    paginator = Paginator('Hub Item', page_number=page_number, fields=fields, filters=filters, order_by='name')
+    context.items = paginator.get_page()
+    context.paginator = paginator
     context.no_breadcrumbs = False
     context.show_search = True
     context.title = "%s %s" % (category_name, 'Products')
