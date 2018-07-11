30eb8d199f110b2881ddcb8c6ae6f072d6651149
Akshar Raaj <akshar@agiliq.com>
2018-07-11 11:46:35 +0530

    Added pagination on company products page

--- hub/www/company_products.py
+++ hub/www/company_products.py
@@ -1,10 +1,16 @@
 import frappe
 
+from hub.paginator import Paginator
+
 def get_context(context):
     company_name = frappe.local.request.args['company_name']
     fields = ['published', 'route', 'image', 'name', 'company_name', 'price', 'stock_qty', 'currency']
     filters = {'published': 1, 'company_name': company_name}
-    context.items = frappe.get_list('Hub Item', fields=fields, filters=filters, start=0, limit=20)
+    page_number = int(frappe.local.request.args.get('page_number', 1))
+    paginator = Paginator('Hub Item', page_number=page_number, fields=fields, filters=filters, order_by='name')
+    context.items = paginator.get_page()
+    context.paginator = paginator
+    context.company_name = company_name
     context.no_breadcrumbs = False
     context.show_search = True
     context.title = "%s %s" % (company_name, 'Products')
