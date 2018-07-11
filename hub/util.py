58d8a6ae2ff8534f49cc67f6687ee249a3ea6bfd
Akshar Raaj <akshar@agiliq.com>
2018-07-11 12:51:04 +0530

    Added a utility function to create tree of categories and their respective sub categories

--- hub/util.py
+++ hub/util.py
@@ -1,5 +1,7 @@
 import json
 
+import frappe
+
 def safe_json_loads(string):
     try:
         string = json.loads(string)
@@ -10,3 +12,19 @@ def safe_json_loads(string):
 
 def assign_if_empty(a, b):
     return a if a else b
+
+def get_categories_and_subcategories():
+    """
+    Return a list of parent categories where each parent category has a key `child_items`.
+    """
+    categories = frappe.get_list('Hub Category', fields=['name', 'parent_hub_category'], filters=[["name", "!=", "All Categories"]])
+    parents = filter(lambda category: category['parent_hub_category'] == 'All Categories', categories)
+    sub_categories = filter(lambda category: category['parent_hub_category'] != 'All Categories', categories)
+    parents_dict = {parent['name']: parent for parent in parents}
+    for parent in parents_dict.values():
+        parent['child_items'] = []
+    for sc in sub_categories:
+        parent_name = sc['parent_hub_category']
+        parent = parents_dict[parent_name]
+        parent['child_items'].append(sc)
+    return parents_dict.values()
