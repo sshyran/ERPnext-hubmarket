5c85766bd9feff27f307e97dc2a9da7f046c1e24
Akshar Raaj <akshar@agiliq.com>
2018-07-20 13:51:09 +0530

    Added query/approach to annotate each item on listing pages with number of reviews

--- hub/www/item_listing.py
+++ hub/www/item_listing.py
@@ -8,6 +8,9 @@ def get_context(context):
     filters = {'published': 1}
     page_number = int(frappe.local.request.args.get('page_number', 1))
     paginator = Paginator('Hub Item', page_number=page_number, fields=fields, filters=filters, order_by='name')
+    # We want to annotate each item with the number of reviews.
+    # Raw query would be "select h.name, count(r.content) as cnt from `tabHub Item` h left outer join `tabHub Item Review` r on h.name=r.parent group by h.name order by cnt desc"
+    # Find how this can be achieved with frappe
     context.items = paginator.get_page()
     context.paginator = paginator
     context.no_breadcrumbs = False
