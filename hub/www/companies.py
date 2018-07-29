0593b953363c4928d55af3adf51c424d72f84bfe
Akshar Raaj <akshar@agiliq.com>
2018-07-29 15:08:23 +0530

    Cleanup

--- hub/www/companies.py
+++ hub/www/companies.py
@@ -2,8 +2,8 @@ import frappe
 
 
 def get_context(context):
-    context.companies = frappe.get_all("Hub Company", fields="name")
+    context.companies = frappe.get_all("Hub Company", fields="name", order_by="name")
     context.products_count = {}
     for company in context.companies:
         # TODO: This makes db call for each company which isn't performant. Keep a field product count on company.
-    	context.products_count[company.name] = frappe.db.count("Hub Item", filters={'company_name':company.name})
+        company['num_products'] = frappe.db.count("Hub Item", filters={'company_name': company.name})
