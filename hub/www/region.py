1ae19ea2fdd8f4a17ff8bb9fd05c09cb7bca2aef
Akshar Raaj <akshar@agiliq.com>
2018-07-29 12:38:56 +0530

    Order by company name inside country group. Use multiple column ordering while using frappe.get_all()

--- hub/www/region.py
+++ hub/www/region.py
@@ -4,5 +4,5 @@ import frappe
 
 
 def get_context(context):
-    companies = frappe.get_all("Hub Company", fields="name, country", order_by="country")
+    companies = frappe.get_all("Hub Company", fields="name, country", order_by="country, name")
     context.companies_grouped_by_country = itertools.groupby(companies, key=lambda x: x['country'])
