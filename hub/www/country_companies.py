80de890279e9861cf89a3296b38c7f92ad4afcc6
Akshar Raaj <akshar@agiliq.com>
2018-07-29 12:57:22 +0530

    use frappe.get_all() instead of frappe.db.get_values().

--- hub/www/country_companies.py
+++ hub/www/country_companies.py
@@ -4,5 +4,5 @@ def get_context(context):
 	"""
 	get the company related to country
 	"""
-	region = frappe.local.request.args.get('name')
-	context.companies = frappe.db.get_values("Hub Company", filters={'country':region}, as_dict=True)
+	country = frappe.local.request.args.get('name')
+	context.companies = frappe.get_all("Hub Company", filters={'country': country})
