d3f1f67cf557f9332820fc05be3c350b5570fd4b
Akshar Raaj <akshar@agiliq.com>
2018-07-29 12:02:39 +0530

    Do not put everything on `context`. Only variable which will be used in template should be added to context.

--- hub/www/region.py
+++ hub/www/region.py
@@ -2,12 +2,10 @@ import frappe
 
 
 def get_context(context):
-	context.companies = frappe.db.get_all("Hub Company", filters={}, fields="country")
-	context.region_list = []
+	companies = frappe.get_all("Hub Company", fields="country")
+	region_list = []
 	context.region_company_list ={}
-	for company in context.companies:
-		if company.country not in context.region_list:
-			context.region_list.append(company.country)
-	for region in context.region_list:
+	region_list = set([company.country for company in companies])
+	for region in region_list:
 		context.region_companies = frappe.db.get_all("Hub Company", filters={'country':region}, fields="name")
 		context.region_company_list[region] = context.region_companies
