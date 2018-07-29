326e6a641c39774675043854c31b23fc16ccabb5
Akshar Raaj <akshar@agiliq.com>
2018-07-29 12:37:02 +0530

    Avoid multiple db calls.
    Make single db calls to find all companies and use itertools.groupby()
    to group them by country.

--- hub/www/region.py
+++ hub/www/region.py
@@ -1,11 +1,8 @@
+import itertools
+
 import frappe
 
 
 def get_context(context):
-	companies = frappe.get_all("Hub Company", fields="country")
-	region_list = []
-	context.region_company_list ={}
-	region_list = set([company.country for company in companies])
-	for region in region_list:
-		context.region_companies = frappe.db.get_all("Hub Company", filters={'country':region}, fields="name")
-		context.region_company_list[region] = context.region_companies
+    companies = frappe.get_all("Hub Company", fields="name, country", order_by="country")
+    context.companies_grouped_by_country = itertools.groupby(companies, key=lambda x: x['country'])
