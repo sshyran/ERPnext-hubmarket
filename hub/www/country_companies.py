6976ef2ac9f15b2daa0d69c8ac4756946077e6a6
Akshar Raaj <akshar@agiliq.com>
2018-07-29 12:55:06 +0530

    typo fix

--- hub/www/country_companies.py
+++ hub/www/country_companies.py
@@ -2,7 +2,7 @@ import frappe
 
 def get_context(context):
 	"""
-	get the company realted to country
+	get the company related to country
 	"""
 	region = frappe.local.request.args.get('name')
 	context.companies = frappe.db.get_values("Hub Company", filters={'country':region}, as_dict=True)
