11a5ab0eff556e1295059437b0e231af7607fa49
Suraj Shetty <surajshetty3416@gmail.com>
2018-08-21 11:27:23 +0530

    Rename patch file and update patches.txt

--- hub/patches/v0_0_1/set_keywords_for_hub_item_if_missing.py
+++ hub/patches/v0_0_1/set_keywords_for_hub_item_if_missing.py
@@ -5,7 +5,6 @@ def execute():
 		'keywords': ''
 	})
 	for item in hub_items:
-		# Any right ways to optimize this? rewrite keyword update logic?
 		doc = frappe.get_doc('Hub Item', item.name)
 		# this will call validate which will update the keyword of the item
 		doc.save()
