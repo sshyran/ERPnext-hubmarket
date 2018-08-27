037998df7460d4c1be6b8ee51ad0d4a3c77aafaa
Suraj Shetty <surajshetty3416@gmail.com>
2018-08-27 14:48:59 +0530

    Fix code mistake

--- hub/patches/v0_0_1/move_name_to_old_name_and_set_random_string_to_name.py
+++ hub/patches/v0_0_1/move_name_to_old_name_and_set_random_string_to_name.py
@@ -4,6 +4,6 @@ def execute():
 	if not frappe.db.has_column('Hub Item', 'old_name'):
 		frappe.reload_doctype('Hub Item')
 		frappe.db.sql('''UPDATE `tabHub Item`
-			SET `old_name` = `name`, `name` = CONCAT(SUBSTRING(`name`, 1, 16), '-', SUBSTRING(MD5(`name`), 1, 12)''')
+			SET `old_name` = `name`, `name` = CONCAT(SUBSTRING(`name`, 1, 16), '-', SUBSTRING(MD5(`name`), 1, 12))''')
 
 		frappe.db.commit()
