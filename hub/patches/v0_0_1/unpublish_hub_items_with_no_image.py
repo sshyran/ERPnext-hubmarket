a054a533213daa42f6077bd4700ba426deb14556
Faris Ansari <netchamp.faris@gmail.com>
2018-09-08 00:46:45 +0530

    fix: Add patch to unpublish items with no image

---
+++ hub/patches/v0_0_1/unpublish_hub_items_with_no_image.py
@@ -0,0 +1,8 @@
+import frappe
+
+def execute():
+	for hub_item in frappe.db.get_all('Hub item', fields=['name', 'image']):
+		if not hub_item.image:
+			frappe.db.set_value('Hub Item', hub_item.name, 'published', 0, update_modified=False)
+
+	frappe.db.commit()
