89fc5fdae77bda9bdbf4b9c19ed41f571af20045
Faris Ansari <netchamp.faris@gmail.com>
2018-09-08 11:18:07 +0530

    fix: typo

--- hub/patches/v0_0_1/unpublish_hub_items_with_no_image.py
+++ hub/patches/v0_0_1/unpublish_hub_items_with_no_image.py
@@ -1,7 +1,7 @@
 import frappe
 
 def execute():
-	for hub_item in frappe.db.get_all('Hub item', fields=['name', 'image']):
+	for hub_item in frappe.db.get_all('Hub Item', fields=['name', 'image']):
 		if not hub_item.image:
 			frappe.db.set_value('Hub Item', hub_item.name, 'published', 0, update_modified=False)
 
