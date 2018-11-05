55c26e9469a19f7adb023cb56ef7b183e5f1f0be
Faris Ansari <netchamp.faris@gmail.com>
2018-11-05 18:42:12 +0530

    fix: Use new file api

--- hub/hub/utils.py
+++ hub/hub/utils.py
@@ -8,7 +8,6 @@ import os
 import requests
 import base64
 from frappe.utils import cint
-from frappe.utils.file_manager import save_file
 
 def autoname_increment_by_field(doctype, field_name, name):
 	count = frappe.db.count(doctype, {field_name: name})
@@ -33,6 +32,14 @@ def save_remote_file_locally(file_url, doctype, name):
 
 	f = None
 	if response.ok:
-		f = save_file(file_name, response.content, doctype, name)
+		f = frappe.get_doc({
+			'doctype': 'File',
+			'file_name': file_name,
+			'attached_to_doctype': doctype,
+			'attached_to_name': name,
+			'content': response.content
+		})
+
+		f.save()
 
 	return f
